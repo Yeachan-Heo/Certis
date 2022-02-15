5a3d7003642f2e84905df1dc00a07d507c8d5301
Yeachan-Heo <54757707+Yeachan-Heo@users.noreply.github.com>
2022-02-15 12:02:49 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,15 +11,15 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-import sys
-sys.path.insert(0, '../../Certis')
+# import sys
+# sys.path.insert(0, '../../Certis')
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'certis'
-copyright = '2021, Author'
-author = 'Author'
+copyright = '2022, Yeachan Heo'
+author = 'Yeachan Heo'
 
 
 # -- General configuration ---------------------------------------------------
